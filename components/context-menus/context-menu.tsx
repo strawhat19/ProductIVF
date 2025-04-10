5c7f4a5b4bf2b40fd5865c88e53a634a93eafc6c
rakib <rahmed@hvac.mea.com>
2025-04-10 02:04:56 -0400

    Custom Right Click Context Menu Adjustments

--- components/context-menus/context-menu.tsx
+++ components/context-menus/context-menu.tsx
@@ -5,7 +5,7 @@ import { fontAwesomeIcons } from '../../shared/constants';
 import { updateDocFieldsWTimeStamp } from '../../firebase';
 
 export default function ContextMenu({ menuRef, menuPosition, iconColor = `var(--gameBlue)` }: any) {
-    let { selected, setMenuPosition, setItemTypeMenuOpen, setSelected } = useContext<any>(StateContext);
+    let { devEnv, selected, setMenuPosition, setItemTypeMenuOpen, setSelected } = useContext<any>(StateContext);
     let ids = (selected == null || selected?.column == undefined || selected?.column == null) ? [] : Array.from(selected?.column?.data?.itemIDs);
 
     const onDismiss = (setSelect = true) => {
@@ -110,21 +110,9 @@ export default function ContextMenu({ menuRef, menuPosition, iconColor = `var(--
                     <i className={`fas fa-copy`} style={{ color: iconColor }} /> <span>Copy</span>
                 </li>
                 <li className={`customContextMenuOption flex gap15`} onClick={() => setItemTaskForm(selected?.item?.options?.showTaskForm)}>
-                    <i className={`fas ${selected?.item?.options?.showTaskForm ? `fa-minus` : `fa-plus`}`} style={{ color: iconColor }} /> <span>{selected?.item?.options?.showTaskForm ? `` : ``}Task Form</span>
+                    <i className={`fas ${selected?.item?.options?.showTaskForm ? `fa-minus` : `fa-plus`}`} style={{ color: iconColor, fontSize: selected?.item?.options?.showTaskForm ? 14 : undefined }} /> <span>{selected?.item?.options?.showTaskForm ? `` : ``}Tasks</span>
                 </li>
-                <li className={`customContextMenuOption flex gap15`} onClick={onArchiveItem}>
-                    <i className={`archivedIcon ${fontAwesomeIcons.folder}`} style={{ color: iconColor }} /> <span>Archive</span>
-                </li>
-                <li className={`customContextMenuOption flex gap15`} onClick={onManageItem}>
-                    <i className={`fas fa-bars`} style={{ color: iconColor }} /> <span>Manage</span>
-                </li>
-                <li className={`customContextMenuOption flex gap15`} onClick={onCompleteItem}>
-                    <i className={`contextMenuIcon ${selected?.item?.options?.complete ? `fas fa-history` : isActiveItem() ? `fas fa-check-circle` : `fas fa-play-circle`}`} style={{ color: iconColor }} /> <span>{selected?.item?.options?.complete ? `Open` : isActiveItem() ? `Complete` : `Active`}</span>
-                </li>
-                <li className={`customContextMenuOption flex gap15`} onClick={onDeleteItem}>
-                    <i className={`fas fa-trash`} style={{ color: iconColor }} /> <span>Delete</span>
-                </li>
-               {ids?.indexOf(selected?.item?.id) > 0 && (
+                {ids?.indexOf(selected?.item?.id) > 0 && (
                     <li className={`customContextMenuOption flex gap15`} onClick={() => moveItemToPosition()}>
                         <i className={`fas fa-sort-amount-up`} style={{ color: iconColor }} /> <span>To Top</span>
                     </li>
@@ -134,6 +122,20 @@ export default function ContextMenu({ menuRef, menuPosition, iconColor = `var(--
                         <i className={`fas fa-sort-amount-down`} style={{ color: iconColor }} /> <span>To Bottom</span>
                     </li>
                 )}
+                {/* {devEnv && (
+                    <li className={`customContextMenuOption flex gap15`} onClick={onArchiveItem}>
+                        <i className={`archivedIcon ${fontAwesomeIcons.folder}`} style={{ color: iconColor }} /> <span>Archive</span>
+                    </li>
+                )} */}
+                {/* <li className={`customContextMenuOption flex gap15`} onClick={onManageItem}>
+                    <i className={`fas fa-bars`} style={{ color: iconColor }} /> <span>Manage</span>
+                </li>
+                <li className={`customContextMenuOption flex gap15`} onClick={onCompleteItem}>
+                    <i className={`contextMenuIcon ${selected?.item?.options?.complete ? `fas fa-history` : isActiveItem() ? `fas fa-check-circle` : `fas fa-play-circle`}`} style={{ color: iconColor }} /> <span>{selected?.item?.options?.complete ? `Open` : isActiveItem() ? `Complete` : `Active`}</span>
+                </li>
+                <li className={`customContextMenuOption flex gap15`} onClick={onDeleteItem}>
+                    <i className={`fas fa-trash`} style={{ color: iconColor }} /> <span>Delete</span>
+                </li> */}
                 {/* <li className={`customContextMenuOption flex gap15`} onClick={() => onRightClick()}>
                     <i className={`fas fa-mouse-pointer`} style={{ color: iconColor }} /> <span>Right Click</span>
                 </li> */}
