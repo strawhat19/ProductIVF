499a3db96d50a0f1a8f13252a3771e38d26c654c
rakib <rahmed@hvac.mea.com>
2025-06-16 01:28:57 -0400

    v6.3.5: Message Images Lazy Load Component

--- components/messages/message/message-preview.tsx
+++ components/messages/message/message-preview.tsx
@@ -3,17 +3,59 @@ import { useContext } from 'react';
 import Avatar from '../avatar/avatar';
 import CustomImage from '../../custom-image';
 import { StateContext } from '../../../pages/_app';
+import parse, { Element } from 'html-react-parser';
 import { Message } from '../../../shared/models/Message';
 
 export function stripHtmlTags(html: string): string {
-    const tempDiv = document.createElement('div');
+    const tempDiv = document.createElement(`div`);
     tempDiv.innerHTML = html;
-    return tempDiv.textContent || tempDiv.innerText || '';
+    return tempDiv.textContent || tempDiv.innerText || ``;
 }
 
 export default function MessagePreview({ userMessage, clickableMsg = true, onClick = () => {} }: any) {
     let { user, users } = useContext<any>(StateContext);
-    // let [message, setMessage] = useState(userMessage);
+
+    const isUserMessage = (userMessage) => {
+        let userIsMsgCreator = userMessage?.creator?.toLowerCase() == user?.email?.toLowerCase();
+        return userIsMsgCreator;
+    }
+
+    const cleanHTML = (htmlString: string) => {
+        return DOMPurify.sanitize(htmlString, {
+            ADD_ATTR: [`target`, `rel`, `id`, `class`],
+            ALLOWED_ATTR: [`href`, `src`, `alt`, `target`, `rel`, `id`, `class`],
+            ALLOWED_TAGS: [`a`, `img`, `p`, `strong`, `em`, `ul`, `li`, `ol`, `br`],
+        });
+    }
+
+    const getMessageTimeStamp = (msg: Message) => {
+        let msgTime = msg?.meta?.updated;
+        if (msgTime) {
+            let msgTimeStampT = msgTime?.split(` `)[0];
+            let msgTimeStampX = msgTime?.split(` `)[1];
+            let msgTimeStamp = `${msgTimeStampT} ${msgTimeStampX}`;
+            return msgTimeStamp;
+        }
+    }
+
+    const renderContent = (htmlString: string) => {
+        let cleanedHTML =  cleanHTML(htmlString);
+        let renderedContent = parse(cleanedHTML, {
+            replace: (domNode) => {
+                if (domNode instanceof Element && domNode.name === `img`) {
+                    const { src, alt, class: className } = domNode.attribs;
+                    return (
+                        <CustomImage
+                            src={src}
+                            alt={alt || `Message Image`}
+                            className={className || `msgImg`}
+                        />
+                    );
+                }
+            },
+        });
+        return renderedContent;
+    }
 
     const getMessageName = (msg: Message) => {
         let msgName = msg?.name;
@@ -35,26 +77,8 @@ export default function MessagePreview({ userMessage, clickableMsg = true, onCli
         return msgName;
     }
 
-    const getMessageTimeStamp = (msg: Message) => {
-        let msgTime = userMessage?.meta?.updated;
-        if (msgTime) {
-            let msgTimeStampT = msgTime?.split(` `)[0];
-            let msgTimeStampX = msgTime?.split(` `)[1];
-            let msgTimeStamp = `${msgTimeStampT} ${msgTimeStampX}`;
-            return msgTimeStamp;
-        }
-    }
-
-    const cleanHTML = (htmlString: string) => {
-        return DOMPurify.sanitize(htmlString, {
-            ADD_ATTR: [`target`, `rel`, `id`, `class`],
-            ALLOWED_ATTR: [`href`, `src`, `alt`, `target`, `rel`, `id`, `class`],
-            ALLOWED_TAGS: [`a`, `img`, `p`, `strong`, `em`, `ul`, `li`, `ol`, `br`],
-        });
-    }
-
     return (
-        <div className={`messagePreview ${clickableMsg ? `clickableMsg` : `nonClickableMsg`} ${userMessage?.creator?.toLowerCase() == user?.email?.toLowerCase() ? `msgCreator` : `msgReader`}`} onClick={onClick}>
+        <div className={`messagePreview ${clickableMsg ? `clickableMsg` : `nonClickableMsg`} ${isUserMessage(userMessage) ? `msgCreator` : `msgReader`}`} onClick={onClick}>
             {userMessage?.image && userMessage?.image != `` ? (
                 <CustomImage 
                     alt={`Avatar Image`} 
@@ -73,12 +97,10 @@ export default function MessagePreview({ userMessage, clickableMsg = true, onCli
                 </div>
                 <div className={`messageContentContainer`}>
                     <div className={`messageContentPreview renderHTML ${clickableMsg ? `clippedText` : `fullText`}`}>
-                        {clickableMsg && (userMessage?.creator?.toLowerCase() == user?.email?.toLowerCase()) ? `you: ` : ``} 
-                        {clickableMsg ? stripHtmlTags(userMessage?.content) : (
-                            <span dangerouslySetInnerHTML={{ __html: cleanHTML(userMessage?.content) }} />
-                        )}
+                        {clickableMsg && (isUserMessage(userMessage)) ? `you: ` : ``} 
+                        {clickableMsg ? stripHtmlTags(userMessage?.content) : renderContent(userMessage?.content)}
                     </div>
-                    {/* {!clickableMsg && (userMessage?.creator?.toLowerCase() == user?.email?.toLowerCase()) && (
+                    {/* {!clickableMsg && (isUserMessage(userMessage)) && (
                         <div className={`messageEndColumn`}>
                             <button title={`Delete Message`} className={`messageActionButton iconButton`}>
                                 <i className={`fas fa-trash`} />
